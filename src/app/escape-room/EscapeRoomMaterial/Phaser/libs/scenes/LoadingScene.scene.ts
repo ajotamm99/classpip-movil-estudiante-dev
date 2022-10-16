1b9113ac986b9b286d896a1d060e7bd3046825b5
ajotamm99 <72545812+ajotamm99@users.noreply.github.com>
2022-10-16 16:42:18 +0200

    Fixes

--- src/app/escape-room/EscapeRoomMaterial/Phaser/libs/scenes/LoadingScene.scene.ts
+++ src/app/escape-room/EscapeRoomMaterial/Phaser/libs/scenes/LoadingScene.scene.ts
@@ -28,6 +28,9 @@ export class LoadingScene extends Phaser.Scene{
     hiddenTimeStamp: number=0;
     tiempoEmpleadoTotalMinutos: number;
     timedEventtest: Phaser.Time.TimerEvent;
+    textTerminado: Phaser.GameObjects.Text;
+    playerReady: boolean=false;
+    juegoTerminado: boolean=false;
 
     constructor() {
         super({ 
@@ -294,7 +297,9 @@ export class LoadingScene extends Phaser.Scene{
                             .then(data=>{
                                 game.Preguntas.push(data);
                                 var imagen= data.Imagen;
-                                game.load.image((cont+1)+'preg', 'http://localhost:3000/api/imagenes/ImagenesPreguntas/download/'+imagen)
+                                if (imagen){
+                                    game.load.image((cont+1)+'preg', 'http://localhost:3000/api/imagenes/ImagenesPreguntas/download/'+imagen)
+                                }
                                 
                                 console.log(data);
                                 cont++;
@@ -379,8 +384,8 @@ export class LoadingScene extends Phaser.Scene{
 
         //Bucle para crear mapas, objetos y colisiones
         for(let c=0; c<this.EscenasActivas.length; c++){            
-            this.maps.push(this.make.tilemap({key:(c+1)+'map'}));
-            this.tilesheets.push(this.maps[c].addTilesetImage('tilesetincial',(c+1)+'tiles'));
+            this.maps.push(this.make.tilemap({key:(c+1).toString()+'map'}));
+            this.tilesheets.push(this.maps[c].addTilesetImage('tilesetincial',(c+1).toString()+'tiles'));
         }
         
         this.layersActivas.push(this.maps[0].createLayer('suelo',this.tilesheets[0],0,0).setDepth(0).setOrigin(0,0));
@@ -412,17 +417,20 @@ export class LoadingScene extends Phaser.Scene{
         //Añadimos el player
         //@ts-ignore
         this.player= this.physics.add.image(this.layersActivas[0].width/2 + 150, this.game.config.height/2,'1skinimg');
+        
         //En este caso lo escalo manualmente, hay que fijarse al subir las skins de que 
         //tienen que ser de un tamaño similar a los tiles de la escena
         this.player.setScale(0.1);
         this.player.setCollideWorldBounds(true);
         this.player.setDepth(1);
         this.player.setOrigin(0,0);
+        this.player.setVelocity(0);
 
         //si queremos modificar el collidebox del sprite
         //this.player.body.setSize(this.player.width, this.player.height/2, false);
         this.physics.add.collider(this.player, this.layersActivas[1]);
         this.cameras.main.startFollow(this.player, true, 0.4, 0.4);
+        this.playerReady=true;
         //this.cameras.add().startFollow(this.cameras.main)
         
         //Texto de tiempo, nota y score
@@ -430,11 +438,11 @@ export class LoadingScene extends Phaser.Scene{
         .setDepth(this.layersActivas.length+2).setScrollFactor(0);
 
         //Función que necesitaremos para re-escalar la mochila y los botones dentro de la pestaña
-        this.scale.on('resize', this.resize, this);
+        
 
         //Configuración timer
         this.timedEvent= this.time.addEvent({ delay: 1000, repeat: this.EscenaEnCurso.TiempoLimite*60, callback: this.onEvent, callbackScope:this });
-        //this.timedEventtest= this.time.addEvent({ delay: 3000, callback: this.testDelete, callbackScope:this });
+        this.timedEventtest= this.time.addEvent({ delay: 3000, callback: this.testDelete, callbackScope:this });
         
         //Timer continua aunque se minimize la pestaña para que no hagan trampas
         
@@ -446,82 +454,94 @@ export class LoadingScene extends Phaser.Scene{
             let elapsedTime = Math.floor((performance.now() - this.hiddenTimeStamp)/1000); //seconds
             this.timerGlobal += elapsedTime;
             })
-    
+
+        this.scale.on('resize', this.resize, this);
     
     }
 
     update(time: number, delta: number): void {
+        //console.log('hola');
         //TIMECONTROLS
-        if (this.timerGlobal>=this.EscenaEnCurso.TiempoLimite*60){
-            this.onEventFinal(true);
-        }else{
-            this.onEventFinal(false);
+        
+        if (this.contadorEscenas==this.EscenasActivas.length){
+            if(this.timerGlobal>=this.EscenaEnCurso.TiempoLimite*60){                
+                this.onEventFinal(true);
+            }else{       
+                //logica cuando completa el juego         
+                //this.onEventFinal(false);
+            }
         }
 
         //NEEDSCONTROL
 
         //PLAYER CONTROLS
-        this.player.setVelocity(0);
+        if(this.playerReady){
+                this.player.setVelocity(0);
 
-        if (this.cursors.left.isDown)
-        {
-            this.player.setVelocityX(-500);
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.player.setVelocityX(500);
-        }
+            if (this.cursors.left.isDown)
+            {
+                this.player.setVelocityX(-500);
+            }
+            else if (this.cursors.right.isDown)
+            {
+                this.player.setVelocityX(500);
+            }
 
-        if (this.cursors.up.isDown)
-        {
-            this.player.setVelocityY(-500);
+            if (this.cursors.up.isDown)
+            {
+                this.player.setVelocityY(-500);
+            }
+            else if (this.cursors.down.isDown)
+            {
+                this.player.setVelocityY(500);
+            }
+
         }
-        else if (this.cursors.down.isDown)
-        {
-            this.player.setVelocityY(500);
-        }
+        
     }
   
     resize (gameSize, baseSize, displaySize, resolution)
     {
 
         var width = gameSize.width;
+        //this.HeightWindow=gameSize.width;
         var height = gameSize.height;
+        //this.widthWindow = height;
         console.log(width, height);
-        this.bag.setX(width);
-        this.bag.setY(height);
-    
-        this.cameras.resize(width, height);
-
-        if(this.layersActivas[0].width<=width && this.layersActivas[0].height<=height){
-            this.bag.setPosition(this.layersActivas[0].width,this.layersActivas[0].height);
-        }else if(this.layersActivas[0].width>=width && this.layersActivas[0].height<=height){            
-            this.bag.setPosition(width,this.layersActivas[0].height-5);
-        }else if(this.layersActivas[0].width<=width && this.layersActivas[0].height>=height){            
-            this.bag.setPosition(this.layersActivas[0].width,height-5);
+        
+        if(this.juegoTerminado){
+            this.textTerminado.setPosition(width/2, height/2);
         }else{
+            this.bag.setX(width);
+            this.bag.setY(height);
+        
+            this.cameras.resize(width, height);
+
+            if(this.layersActivas[0].width<=width && this.layersActivas[0].height<=height){
+                this.bag.setPosition(this.layersActivas[0].width,this.layersActivas[0].height);
+            }else if(this.layersActivas[0].width>=width && this.layersActivas[0].height<=height){            
+                this.bag.setPosition(width,this.layersActivas[0].height-5);
+            }else if(this.layersActivas[0].width<=width && this.layersActivas[0].height>=height){            
+                this.bag.setPosition(this.layersActivas[0].width,height-5);
+            }else{                
+                this.bag.setPosition(width,height-5);
+            }
             
-            this.bag.setPosition(width,height-5);
         }
-    
-        /*for(let i=0; i<this.layersActivas.length; i++){
-            this.layersActivas[i].setSize(width, height);
-        }*/
-        //this.layer1.setSize(width, height);
-        //this.solid.setSize(width, height);
     }
 
     onEvent(){
-        this.timerGlobal+=1;
-        this.tiempoActual=this.EscenaEnCurso.TiempoLimite*60 - this.timerGlobal;
-        var min=this.tiempoActual/60 | 0;
-        var sec= this.tiempoActual % 60;
-        var secStr: string =sec.toString();
-        if (sec<10){
-            secStr='0'+sec;
+        if(!this.juegoTerminado){
+            this.timerGlobal+=1;
+            this.tiempoActual=this.EscenaEnCurso.TiempoLimite*60 - this.timerGlobal;
+            var min=this.tiempoActual/60 | 0;
+            var sec= this.tiempoActual % 60;
+            var secStr: string =sec.toString();
+            if (sec<10){
+                secStr='0'+sec;
+            }
+            this.textEscenas.text='Escena: '+this.contadorEscenas+'/'+this.EscenasActivas.length+'\n'+'Nota: '+this.nota +'\n'+'Tiempo disponible: '+ min+':'+secStr;
         }
-        this.textEscenas.text='Escena: '+this.contadorEscenas+'/'+this.EscenasActivas.length+'\n'+'Nota: '+this.nota +'\n'+'Tiempo disponible: '+ min+':'+secStr;
-
     }
 
     onEventFinal(pasado: boolean){
@@ -535,7 +555,7 @@ export class LoadingScene extends Phaser.Scene{
                 this.tiempoEmpleadoTotal+=this.timerGlobal;
                 this.tiempoEmpleadoTotalMinutos=this.tiempoEmpleadoTotal/60 | 0;
             }
-            this.juegoTerminado();
+            this.JuegoTerminado();
             //Pantalla Juego terminado + fetch post
         }else{
             
@@ -554,11 +574,6 @@ export class LoadingScene extends Phaser.Scene{
             this.changeScene();
             this.timedEvent=this.time.addEvent({ delay: 1000, repeat: this.EscenaEnCurso.TiempoLimite*60, callback: this.onEvent, callbackScope:this });
         
-            
-            //this.mapSelected.destroy();
-            //for(let i=0; i<this.layersActivas.length; i++){
-                //this.layersActivas[i].destroy();
-            //}
             this.textEscenas.text='Escena: '+this.contadorEscenas+'/'+this.EscenasActivas.length+'\n'+'Nota: '+this.nota +'\n'+'Tiempo disponible: '+ this.tiemposEscenas[this.contadorEscenas-1]+':00';
         
 
@@ -567,6 +582,8 @@ export class LoadingScene extends Phaser.Scene{
     }
 
     changeScene(){
+        
+        this.playerReady=false;
         this.player.removeAllListeners().destroy();
         
         this.physics.world.colliders.destroy();    
@@ -603,25 +620,46 @@ export class LoadingScene extends Phaser.Scene{
         //si queremos modificar el collidebox del sprite
         //this.player.body.setSize(this.player.width, this.player.height/2, false);
         this.physics.add.collider(this.player, this.layersActivas[1]);
+        this.playerReady=true;
         this.cameras.main.startFollow(this.player, true, 0.4, 0.4);
     }
 
     testDelete(){
-        this.physics.world.colliders.destroy();    
+        /*this.physics.world.colliders.destroy();    
 
         for(let i=0; i<this.layersActivas.length; i++){
             this.layersActivas[i].removeAllListeners().destroy();
         }
         
         this.layersActivas=[];
-        //this.maps[this.contadorEscenas-2].destroy();
+        //this.maps[this.contadorEscenas-2].destroy();*/
+        this.JuegoTerminado();
     }
 
-    juegoTerminado(){
+    JuegoTerminado(){
+        this.playerReady=false;
+        this.cursors=undefined;
+        this.juegoTerminado=true;
+        
+        this.physics.world.colliders.destroy(); 
+        this.player.removeAllListeners().destroy();
+        
+        this.textEscenas.destroy();
+        
+        for(let i=0; i<this.layersActivas.length; i++){
+            this.layersActivas[i].removeAllListeners().destroy();
+        }
+        this.layersActivas=[];
+        this.bag.destroy();
+        //destroy gamepad too
 
+        var config: Phaser.Types.GameObjects.Text.TextStyle= {align: 'center'};
+        this.textTerminado= this.add.text(this.widthWindow/2, this.HeightWindow/2, 'JUEGO TERMINADO \n \n'
+        +'Nota Final: '+ this.nota+'\n' +'Tiempo Empleado: '+ this.tiempoEmpleadoTotalMinutos, config).setOrigin(0.5,0.5);
+        this.cameras.main.startFollow(this.textTerminado);
     }
 
-    checkRequisitos(){
+    CheckRequisitos(){
 
     }
   }
